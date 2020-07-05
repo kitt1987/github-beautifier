985c2eed132b7af686e05ceb2a1317e5453060c0
kh <kitt.hsu@gmail.com>
2020-07-05 23:53:40 +0800

    disable it

--- background.js
+++ background.js
@@ -1,92 +1,49 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.local.set(Config.Data);
-	console.log("initial configuration:");
-	console.log(Config.Data);
-	// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	// 	console.log("tab" + tabId + "updated");
-	// 	if (!tab.active) {
-	// 		return;
-	// 	}
+// var declarativeContent = chrome.declarativeContent;
+// var PageStateMatcher = declarativeContent.PageStateMatcher;
+// var SetIcon = declarativeContent.SetIcon;
 
-	// 	if (changeInfo.status != "loading") {
-	// 		return;
-	// 	}
+// var iconRule = {
+//     conditions: [
+//       new PageStateMatcher({
+//         pageUrl: { hostSuffix: 'github.com' },
+//       })
+//     ],
+//     actions: [ new SetIcon({
+    	// path: {
+    	// 	"16": "icon/active32.png",
+		   //  "32": "icon/active32.png",
+		   //  "48": "icon/active64.png",
+		   //  "128": "icon/active128.png"
+    	// },
+//     }) ]
+// };
 
-	// 	if (tab.url != null && tab.url.match(/https?:\/\/.*\.?github\.com.*/)) {
-	// 		chrome.tabs.insertCSS(tabId, {
-	// 			code: `.UnderlineNav-body {
-	// 				padding-left: 256px;
-	// 			}`,
-	// 		}, null);
-	// 	}
-	// });
+// var context = this;
+// var onPageChanged = chrome.declarativeContent.onPageChanged;
+// onPageChanged.removeRules(undefined, function() {
+// 	onPageChanged.addRules.apply(context, [iconRule]);
+// });
 
-	chrome.storage.onChanged.addListener(
- 		(changes, areaName) => {
- 			if (areaName != "local") {
- 				return;
- 			}
-
- 			console.log(changes.oldValue);
- 			console.log(changes.newValue);
- 			Config.Data = changes.newValue;
- 			for (let i = 0; i < Config.Modules.length; i++) {
- 				if (changes.oldValue[Config.Modules[i].Name] == changes.newValue[Config.Modules[i].Name]) {
- 					continue;
- 				}
-
- 				if (changes.newValue[Config.Modules[i].Name]) {
- 					// setup content script rule
- 					chrome.declarativeContent.onPageChanged.removeRules(Config.Modules[i].RuleID, () => {
-				    	chrome.declarativeContent.onPageChanged.addRules([Config.Modules[i].CreateContentRule()], (rule) => {
-				    		Config.Modules[i].RuleID = rule.id;
-				    	});
-				    });
-
-				    // apply to tabs
-				    chrome.tabs.query({ url: '*://*.github.com/*'}, (tabs) => {
-				    	for (let j = 0; j < tabs.length; j++) {
-				    		chrome.tabs.executeScript(tabs[j].id, { file: Config.Modules[i].ContentScript});
-				    	}
-				    });
- 					continue
- 				}
-
- 				// FIXME disable it
- 				chrome.declarativeContent.onPageChanged.removeRules(Config.Modules[i].RuleID);
- 				// apply to tabs, run disable script
- 				chrome.tabs.query({ url: '*://*.github.com/*'}, (tabs) => {
-			    	for (let j = 0; j < tabs.length; j++) {
-			    		chrome.tabs.executeScript(tabs[j].id, { code: Config.ContentDisableFuncName(Config.Modules[i]) });
-			    	}
-			    });
- 			}
- 		}
- 	);
-
- 	for (let i = 0; i < Config.Modules.length; i++) {
- 		if (Config.Data[Config.Modules[i].Name]) {
- 			chrome.declarativeContent.onPageChanged.removeRules(Config.Modules[i].RuleID, () => {
-		    	chrome.declarativeContent.onPageChanged.addRules([Config.Modules[i].CreateContentRule()], (rules) => {
-		    		Config.Modules[i].RuleID = rules[0].id;
-		    		console.log("load rules for decorator " + Config.Modules[i].Name + ":" + Config.Modules[i].RuleID);
-		    	});
-		    });
- 		}
-	}
-
- 	// chrome.runtime.onMessage.addListener(
- 	// 	function(message, sender) { 
- 	// 		console.log("got message " + message);			
- 	// 		switch (message) {
- 	// 			case 'init':
- 	// 				Config.LoadAndInitDecorator(sender.tab.id);
- 	// 				break;
-
- 	// 			default:
- 	// 				console.log('unknown message ' + message);
- 	// 				return;
- 	// 		}
- 	// 	}
- 	// );
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	// activeInfo.tabId  activeInfo.windowId
+	chrome.tabs.get(activeInfo.tabId, updateIcon);
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	updateIcon(tab);
+});
+
+function updateIcon(tab) {
+	const u = new URL(tab.url);
+	if (u.hostname.endsWith('github.com')) {
+		chrome.pageAction.setIcon({
+			tabId: tab.id, 
+			path: {
+		          "16": "icon/active32.png",
+		          "32": "icon/active32.png",
+		          "48": "icon/active64.png",
+		          "128": "icon/active128.png"
+		    }
+		});
+	}
+}
