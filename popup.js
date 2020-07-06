0c5933f98368dfac1f1eaef5a38cb45d67192b8b
kh <kitt.hsu@gmail.com>
2020-07-06 10:08:08 +0800

    popup & button style

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-
+window.onload = () => {
+	document.getElementById("repo").addEventListener('click', (event) => {
+		chrome.tabs.create({ url: "https://github.com/kitt1987/github-beautifier" });
+	});
+};
