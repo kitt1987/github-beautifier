3b0b5528e37973b9a382405d9740586be3bcdbac
kh <kitt.hsu@gmail.com>
2020-07-06 09:10:14 +0800

    Use css injection

--- background.js
+++ background.js
@@ -1,29 +1,3 @@
-// var declarativeContent = chrome.declarativeContent;
-// var PageStateMatcher = declarativeContent.PageStateMatcher;
-// var SetIcon = declarativeContent.SetIcon;
-
-// var iconRule = {
-//     conditions: [
-//       new PageStateMatcher({
-//         pageUrl: { hostSuffix: 'github.com' },
-//       })
-//     ],
-//     actions: [ new SetIcon({
-    	// path: {
-    	// 	"16": "icon/active32.png",
-		   //  "32": "icon/active32.png",
-		   //  "48": "icon/active64.png",
-		   //  "128": "icon/active128.png"
-    	// },
-//     }) ]
-// };
-
-// var context = this;
-// var onPageChanged = chrome.declarativeContent.onPageChanged;
-// onPageChanged.removeRules(undefined, function() {
-// 	onPageChanged.addRules.apply(context, [iconRule]);
-// });
-
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 	// activeInfo.tabId  activeInfo.windowId
 	chrome.tabs.get(activeInfo.tabId, updateIcon);
@@ -34,6 +8,10 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 function updateIcon(tab) {
+	if (tab.url == '') {
+		return;
+	}
+
 	const u = new URL(tab.url);
 	if (u.hostname.endsWith('github.com')) {
 		chrome.pageAction.setIcon({
